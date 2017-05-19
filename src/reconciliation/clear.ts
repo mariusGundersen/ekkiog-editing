4ef79a2f943d2ecfe4a68d5700c80114f9bf0d96
Marius Gundersen <me@mariusgundersen.net>
2017-05-19 09:25:35 +0200

    Fixed bugs caused by setting gates to ground
    There really wasn't any need to do this, and it has caused some really weird bugs, so not doing it anymore

--- src/reconciliation/clear.ts
+++ src/reconciliation/clear.ts
@@ -19,13 +19,10 @@ export default function clear(context : MutableContext, change : ChangeClear<Ite
   clearArea(context, change);
   switch(change.before.type){
     case WIRE:
-      return;
     case GATE:
-      return context.setGate(change.before.net, GROUND, GROUND);
     case UNDERPASS:
-      return;
     case BUTTON:
-      return context.setGate(change.before.net, GROUND, GROUND);
+      return;
     case COMPONENT:
       return clearComponent(context, change.before);
   }
@@ -41,9 +38,5 @@ export function clearArea(context : MutableContext, {top, left, width, height} :
 }
 
 export function clearComponent(context : MutableContext, component : Component){
-  for(const gate of component.gates){
-    context.setGate(gate.net, GROUND, GROUND);
-  }
-
   context.removeText(component);
 }
