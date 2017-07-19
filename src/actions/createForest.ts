53c7df92c7d17087c27ba50ab245e81beccf5ea4
Marius Gundersen <me@mariusgundersen.net>
2017-07-19 23:31:17 +0200

    Create a forest based on an existing buddyTree

--- src/actions/createForest.ts
+++ src/actions/createForest.ts
@@ -1,11 +1,10 @@
 import {createTree as createEnneaTree} from 'ennea-tree';
-import {createTree as createBuddyTree, allocate} from 'buddy-tree';
+import {createTree as createBuddyTree, allocate, Node} from 'buddy-tree';
 
 import { Forest, TreeNode } from '../types';
-
-export default function createForest() : Forest{
+export default function createForest(buddyTree = allocate(createBuddyTree(256*256), 2).tree) : Forest {
   return {
     enneaTree: createEnneaTree(128) as TreeNode,
-    buddyTree: allocate(createBuddyTree(256*256), 2).tree
+    buddyTree
   };
 }
